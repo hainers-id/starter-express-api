66654f9e8dd55b5e2014acdcae7b628f8ec7ff66
Khairul Hidayat <me@khairul.my.id>
2023-07-24 05:09:57 +0700

    feat: test check headers

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const app = express();
+app.all("/", (req, res) => {
+  console.log("Just got a request!");
+  res.json({
+    ip: req.ip,
+    headers: req.headers,
+  });
+});
+app.listen(process.env.PORT || 3000);
